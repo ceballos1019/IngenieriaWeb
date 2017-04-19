93e97f7c4c8bef1029111fa37a74ef7f4f048348
Andres Ceballos Sanchez <andresrxn10@hotmail.com>
2017-04-18 22:50:33 -0500

    Implementado el metodo validar en la clase UsuarioBL

--- EjemploSpring/src/co/edu/udea/iw/bl/UsuarioBL.java
+++ EjemploSpring/src/co/edu/udea/iw/bl/UsuarioBL.java
@@ -1,18 +1,18 @@
-/**
- * 
- */
 package co.edu.udea.iw.bl;
 
 import co.edu.udea.iw.dao.UsuarioDAO;
 import co.edu.udea.iw.dto.Usuario;
 import co.edu.udea.iw.exception.MyException;
+import co.edu.udea.iw.util.encode.Cifrado;
 
 /**
+ * Se definen metodos que garantizan la lógica del negocio en las operaciones de la base de datos relacionadas con el Usuario
  * @author Andres Ceballos Sanchez - andres.ceballoss@udea.edu.co
  * @version 1.0
  */
 public class UsuarioBL {
 	
+	/* DAO del usuario que es inyectado por Spring*/
 	private UsuarioDAO usuarioDAO;	
 	
 	/**
@@ -22,32 +22,48 @@ public class UsuarioBL {
 		return usuarioDAO;
 	}
 
-
-
 	/**
 	 * @param usuarioDAO the usuarioDAO to set
 	 */
 	public void setUsuarioDAO(UsuarioDAO usuarioDAO) {
 		this.usuarioDAO = usuarioDAO;
 	}
-
-	public void validar(String login, String password) throws MyException {
+	
+	/**
+	 * Verifica si el usuario y contraseña ingresados son válidos
+	 * @param login login del usuario
+	 * @param password contraseña del usuario
+	 * @return si el usuario existe y la contraseña es correcta, retorna True
+	 * @throws MyException ocurre cuando el usuario no existe o cuando la contraseña es incorrecta
+	 */
+	public Boolean validar(String login, String password) throws MyException {
 		
+		Usuario usuario = null;
+		Cifrado cifrado = null;
+		
+		/*Validar que no sean vacios los campos ingresados*/
 		if(login.isEmpty() || login == null) {
 			throw new MyException("El login no puede estar vacio");
 		}
 		
 		if(password.isEmpty() || password == null) {
-			throw new MyException("La contraseÃ±a no puede estar vacia");
+			throw new MyException("La contraseña no puede estar vacia");
 		}
 		
-		Usuario usuario = usuarioDAO.obtener(login);
+		usuario = usuarioDAO.obtener(login); 
+		
+		/*Verificar que el usuario exista en la base de datos*/
 		if(usuario == null) {
-			throw new MyException("El usuario o la contraseÃ±a es incorrecta");
+			throw new MyException("El usuario o la contraseña es incorrecta");
 		}
 		
-		if(!password.equals(usuario.getContrasena())) {
-			throw new MyException("El usuario o la contraseÃ±a es incorrecta");
+		cifrado = new Cifrado();
+		
+		/*Validar que la contraseña ingresada corresponda con la contraseña en la base de datos*/ 
+		if(!cifrado.encrypt(password).equals(usuario.getContrasena())) {
+			throw new MyException("El usuario o la contraseña es incorrecta");
 		}
+		
+		return Boolean.TRUE; 
 	}
 }
